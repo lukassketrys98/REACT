e8f96d54cbca7e2dcca622a6d6ff3e20f72f8974
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-07-02 15:17:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, {Component} from "react"
+import GrandParent from "./GrandParent"
 
-
-function App() {
-    return (
-        <div>
-            aa
-        </div>
-    )
+class App extends Component {
+    state = { count: 0 }
+    
+    increment = () => this.setState(prevState => ({count: prevState.count + 1}))
+    
+    render() {
+        console.log("[GP] [P] [C] [GC] APP just rendered")
+        return (
+            <div>
+                <button onClick={this.increment}>+1</button>
+                <h2>{this.state.count}</h2>
+                <p>I'm the App component</p>
+                <GrandParent />
+                <GrandParent />
+            </div>
+        )    
+    }
 }
 
 export default App
